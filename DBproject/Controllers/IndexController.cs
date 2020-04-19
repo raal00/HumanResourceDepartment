9c9161df3cbdd99457fc2963b5025681cac3d26f
Raal00 <ivan.pyzhyznov.90@mail.ru>
2020-04-19 18:58:33 +0300

    Добавьте файлы проекта.

---
+++ DBproject/Controllers/IndexController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace DBproject.Controllers
+{
+    public class IndexController
+    {
+        string connectionstring = @"Data Source =.\SQLEXPRESS;Initial Catalog = HumanResourcesDepartmentDD; Integrated Security = True";
+        
+        public void GetData() 
+        {
+            
+        }
+    }
+}
---
+++ DataBaseAPILib/DataBaseController.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace DataBaseAPILib
+{
+    public class DataBaseController
+    {
+        string connectionstring = @"Data Source =.\SQLEXPRESS;Initial Catalog = HumanResourcesDepartmentDD; Integrated Security = True";
+        SqlConnection a;
+    }
+}
