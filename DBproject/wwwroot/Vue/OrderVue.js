b58b5ef66d80fdd6124900a3a29c8e184012b2b8
Raal00 <ivan.pyzhyznov.90@mail.ru>
2020-04-22 20:36:50 +0300

    Подключение vue Представление для приказов и табеля

---
+++ DBproject/wwwroot/Vue/OrderVue.js
@@ -0,0 +1,16 @@
+﻿var appVue = new Vue({
+	el: '#appVue',
+	data:
+	{
+		email: "",
+		password: ""
+	},
+
+	methods:
+	{
+		AlertText: function () {
+			this.email = this.email.split(' ').reverse().join(' ');
+			alert(this.email);
+		}
+	}
+})
